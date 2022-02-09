7d781b16b15e12e4fb1a1c6a6f02006f242ffdf1
Adedapo Adedamola <adedadamola@gmail.com>
2022-02-09 10:06:39 +0100

    api_service_version view

--- APISERVICE/service/views.py
+++ APISERVICE/service/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""
+service/views.py
+"""
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def api_service_version(request):
+    """
+    return the api-service version
+
+    :param request: argument.
+    :type request: object
+
+    :rtype: json
+    :return: api-service version. "V1"
+    """
+    if request.method == "GET":
+        return Response({"data": request.version})
