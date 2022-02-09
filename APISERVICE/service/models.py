6e4736572a33879b89ccfc4f3fb6354178ebb793
Adedapo Adedamola <adedadamola@gmail.com>
2022-02-09 10:00:23 +0100

    Define the db schema for the service app

--- APISERVICE/service/models.py
+++ APISERVICE/service/models.py
@@ -1,8 +1,18 @@
-from ipaddress import ip_address
-from pyexpat import model
+"""
+service/models.py
+"""
+
 from django.db import models
 
 
 class Service(models.Model):
+    """
+    Defines the Service database schema.
+
+    Fields:
+        ip_address (str): server/system ip address.
+        mac_addres (str): server/system mac address.
+    """
+
     ip_address = models.CharField(max_length=15)
     mac_address = models.CharField(max_length=17)
